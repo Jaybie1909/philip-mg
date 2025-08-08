c10c50dd34ec841c3c8019663868ceedbb614bc5
Jaybie1909 <jaybie1909@gmail.com>
2025-08-09 04:44:10 +0800

    Fix path import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // makes image paths relative for Netlify
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './', // ✅ Relative paths for Netlify
 })
